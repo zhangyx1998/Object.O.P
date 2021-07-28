76c538b4d5ad3cb1e8522ec6e43df78c56d6c9f9
Yuxuan Zhang <yuxuan@yuxuanzhang.net>
2021-07-28 09:34:27 +0800

    PA1 Update

--- PA1/MaxProduct.java
+++ PA1/MaxProduct.java
@@ -18,48 +18,67 @@ import java.util.Scanner;
 public class MaxProduct {
     public static long product = 0, maxProduct = 0;
 
-    public static class RotateMemory {
+    public static class RotateQueue {
         private int length, ptr, data[];
         private long product = 0;
 
-        public RotateMemory(int _length) {
+        public RotateQueue(int _length) {
             ptr = 0;
             length = _length;
             data = new int[_length];
         }
 
         public long rotate(int next) {
+            // Store popped element in a temporary variable.
             int tmp = data[ptr];
+            //
             data[ptr] = next;
-            if (product > 0){
+            if (++ptr >= length)
+                // Increment queue pointer, move to start if the pointer exceeds queue boundary.
+                ptr = 0;
+            // Determine the new queue multiplication product.
+            if (product > 0)
+                // When the current product is not 0, all its members are not 0.
                 product = next * product / tmp;
-            } else {
+            else if (tmp != 0)
+                // When the product is 0 while the popped element is not 0, it means that at
+                // least one of its remaining elements is 0, which makes the new product
+                // remaining to be 0.
+                return product = 0;
+            else {
+                // If the popped element is 0, it is possible that the new product is not 0. In
+                // this case, we have to scan the new queue elements to find any other zero
+                // element.
+                for (int i = 0; i < length; i++) {
+                    if (data[i] == 0)
+                        return product = 0;
+                }
+                // Now we are sure that the new queue has no zero elements, hence we should
+                // re-calculate the new queue product.
                 product = 1;
-                for (int i = 0; i < length; i++){
+                for (int i = 0; i < length; i++) {
                     product *= data[i];
                 }
             }
-            if (++ptr >= length)
-                ptr = 0;
             return product;
         }
 
-        public RotateMemory dump(){
-            RotateMemory tmp = new RotateMemory(length);
-            for (int i = 0; i < length; i ++){
+        public RotateQueue dump() {
+            RotateQueue tmp = new RotateQueue(length);
+            for (int i = 0; i < length; i++) {
                 tmp.rotate(data[(ptr + i) % length]);
             }
             return tmp;
         }
 
-        public void print(){
-            print(" ");
+        public void print() {
+            this.print(" ");
         }
 
-        public void print(String str){
-            for (int i = 0; i < length; i ++){
-                if (i > 0){
-                    System.out.print(str);
+        public void print(String conString) {
+            for (int i = 0; i < length; i++) {
+                if (i > 0) {
+                    System.out.print(conString);
                 }
                 System.out.print(data[(ptr + i) % length]);
             }
@@ -70,28 +89,27 @@ public class MaxProduct {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int rotate_length = Integer.parseInt(args[0]);
-        RotateMemory ram = new RotateMemory(rotate_length), maxRam = ram;
-        String str = sc.nextLine();
+        RotateQueue queue = new RotateQueue(rotate_length), maxQueue = queue;
+        String conString = sc.nextLine();
         int i = 0;
-        while (str.length() > 0){
-            if (i >= str.length()){
+        while (conString.length() > 0) {
+            if (i >= conString.length()) {
                 if (!sc.hasNext())
                     break;
-                if ((str = sc.nextLine().trim()).isEmpty())
+                if ((conString = sc.nextLine().trim()).isEmpty())
                     break;
                 i = 0;
             }
-            int nextInt = str.charAt(i) - '0';
-            product = ram.rotate(nextInt);
-            if (product > maxProduct){
-                maxProduct  = product;
-                maxRam = ram.dump();
+            int nextInt = conString.charAt(i) - '0';
+            product = queue.rotate(nextInt);
+            if (product > maxProduct) {
+                maxProduct = product;
+                maxQueue = queue.dump();
             }
             i ++;
-            // input_length --;
         }
         System.out.print(maxProduct + " = ");
-        maxRam.print(" * ");
+        maxQueue.print(" * ");
         System.out.println();
         sc.close();
     }
